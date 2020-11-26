9e6e61738ce179b88875388d851c6bb6f9afda32
wsxc94 <wsxc94@naver.com>
2020-11-26 22:04:01 +0900

    프로그래머스

--- Algorithm/level2영어끝말잇기/영어끝말잇기.cpp
+++ Algorithm/level2영어끝말잇기/영어끝말잇기.cpp
@@ -6,24 +6,27 @@
 using namespace std;
 
 vector<int> solution(int n, vector<string>& words) {
-	vector<int> answer(2);
+	vector<int> answer(2, 0);
 	unordered_map<string, int> maps;
 	char tmp = words[0][words[0].size() - 1];
 	maps[words[0]]++;
 
-	for (int i = 1; i < words.size(); i++) {
+	int num = 2, turn = 1;
 
+	for (int i = 1; i < words.size(); i++) {
+		if (num > n) {
+			num = 1;
+			turn++;
+		}
 		if (maps[words[i]] > 0 || tmp != words[i][0])
 		{
-			cout << n << "\n";
-			answer[0] = (i + 1) / n;
-			answer[1] = (i + 1) % n;
-			break;
+			answer[0] = num;
+			answer[1] = turn;
+			return answer;
 		}
-		 maps[words[i]]++;
-		 tmp = words[i][words[i].size() - 1];
-
-       
+		maps[words[i]]++;
+		tmp = words[i][words[i].size() - 1];
+		num++;
 	}
 
 	return answer;
@@ -34,6 +37,9 @@ int main() {
 		"mother", "robot", "tank" };
 	int n = 3;
 
+	//vector<string> tmp = { "hello" , "one" , "even" , "never" , "now" , "world" , "draw" };
+	
+	//vector<int> s = solution(2, tmp);
 	vector<int> s = solution(n, w);
 
 	for (int i = 0; i < s.size(); i++)
