cdb9d6eb644894e7b1884a2659b0918e68d4278c
안영훈 <wsxc9468@gmail.com>
2020-12-02 12:19:04 +0900

    dp

--- Algorithm/2156/포도주시식.cpp
+++ Algorithm/2156/포도주시식.cpp
@@ -10,23 +10,28 @@ int main() {
 	int n, ans = 0;
 	cin >> n;
 
-	vector<int> arr(n);
-	vector<int> dp(n);
+	vector<int> arr(n + 1);
+	vector<int> dp(n + 1);
 
-	for (int i = 0; i < n; i++)
+	for (int i = 1; i <= n; i++)
 	{
 		cin >> arr[i];
 	}
 
-	dp[0] = 6;
-	dp[1] = arr[0] + arr[1];
+	dp[0] = arr[0] = 0;
+	dp[1] = arr[1];
+	dp[2] = arr[1] + arr[2];
 
-	for (int i = 2; i < n; i++)
+	//3ÀÜ ¿¬¼Ó ¸¶½Ç¼ö ¾ø±â ¶§¹®¿¡ 3ÀÜÀÇ °æ¿ì°¡ ÀÖ´Ù.
+	//1. -3¹øÂ° ÀÜ¸¶½Ã°í -2¹øÂ° ÀÜ °Ç³Ê¶Ù°í -1¹øÂ° ¾ÕÀÜ + ³»ÀÜ
+	//2. -2¹øÂ° ÀÜ ¸¶½Ã°í ³»ÀÜ¸¶½Ã±â
+	//3.³»ÀÜ¾È¸Ô°í -1¹øÂ°ÀÜÀÇ ÃÖ´ë°ª ¸¶½Ã±â
+	for (int i = 3; i <= n; i++)
 	{
-		dp[i] = max(dp[i - 2] + arr[i], dp[i - 1] - arr[i - 2]);
-		ans = max(ans, dp[i]);
-	}
+		dp[i] = max(dp[i - 3] + arr[i - 1] + arr[i], max(dp[i - 2] + arr[i], dp[i - 1]));
 
+	}
+	cout << dp[n] << "\n";
 
 	return 0;
 }
