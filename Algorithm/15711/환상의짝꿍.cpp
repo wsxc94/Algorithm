b4edae534c3a83320f6b380e76e51f942014789b
wsxc94 <wsxc94@naver.com>
2020-12-15 19:38:39 +0900

    소수판별응용

--- Algorithm/15711/환상의짝꿍.cpp
+++ Algorithm/15711/환상의짝꿍.cpp
@@ -9,43 +9,49 @@
 #include <cmath>
 using namespace std;
 
+constexpr int MAX_NUM = 2000000;
+vector<int> primeNum;
+vector<bool> check(MAX_NUM + 1, false);
 long long n, m, sum;
 
-long long power(long long x, long long y, long long mod) {
-	long long res = 1;
-	x %= mod;
-	while (y) {
-		if (y % 2) res = (res * x) % mod;
-		y /= 2;
-		x = (x*x) % mod;
-	}
-	return res;
-}
+bool isPrime(long long num) {
 
-//if n is prime, return true
-bool miller(long long n, long long a) {
-	if (a % n == 0) return false;
-	long long k = n - 1;
-	while (1) {
-		long long temp = power(a, k, n);
-		if (temp == n - 1) return true; //a^k = -1 (mod n)
-		if (k % 2) return (temp == 1 || temp == n - 1);
-		k /= 2;
+	if (num <= MAX_NUM) return !check[num];
+	else {
+		for (int i = 0; i < primeNum.size(); i++)
+			if (num % primeNum[i] == 0)
+				return false;
+		return true;
 	}
 }
-
 int main() {
 	ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
 
 	int T;
 	cin >> T;
 
+	check[0] = check[1] = true;
+	for (int i = 2; i <= MAX_NUM; i++)
+	{
+		if (!check[i]) {
+			primeNum.push_back(i);
+			for (int j = i + i; j <= MAX_NUM; j += i)
+			{
+				check[j] = true;
+			}
+		}
+	}
+
 	while (T--)
 	{
 		cin >> n >> m;
-		
-		if (miller(n, m)) cout << "YES" << "\n";
-		else cout << "NO" << "\n";
+		sum = n + m;
+		if (sum == 2) cout << "NO\n";
+		else if (sum % 2 == 0) cout << "YES\n";
+		else {
+			if (isPrime(sum - 2)) cout << "YES\n";
+			else cout << "NO\n";
+		}
 
 	}
 	return 0;
