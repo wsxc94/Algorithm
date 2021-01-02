312ec3cd9f59634a98a2d8e9846a43a807a55862
wsxc94 <wsxc9468@gmail.com>
2021-01-02 20:19:44 +0900

    bfs 기존꺼 최적화

--- Algorithm/5014/스타트링크.cpp
+++ Algorithm/5014/스타트링크.cpp
@@ -9,52 +9,52 @@
 using namespace std;
 
 int F, S, G, U, D;
+bool ch[1000001];
+queue<int> q;
 
-constexpr int INF = 987654321;
+int bfs() {
+	int res = 0;
+	q.push(S);
+	ch[S] = true;
 
-vector<int> dist(10000001, INF);
+	while (!q.empty())
+	{
+		int qSize = q.size();
+		for (int i = 0; i < qSize; i++)
+		{
+			int present = q.front(); q.pop();
 
+			if (present == G) return res;
+
+			int next = present + U;
+			if (1 <= next && next <= F && !ch[next])
+			{
+				ch[next] = true;
+				q.push(next);
+			}
+			next = present - D;
+			if (1 <= next && next <= F && !ch[next])
+			{
+				ch[next] = true;
+				q.push(next);
+			}
+		}
+		res++;
+	}
+	return -1;
+}
 int main() {
 	ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
 
 	//전체층 현재 스타트링크 위로몇층 아래로몇충
 	cin >> F >> S >> G >> U >> D;
 
+	
+	int ans = bfs();
 
-	priority_queue<int> q;
+	if (ans == -1) cout << "use the stairs\n";
+	else cout << ans << "\n";
 
-	q.push(S);
-	dist[S] = 0;
-
-	while (!q.empty())
-	{
-		int cur = q.top();
-		int up = q.top() + U;
-		int down = q.top() - D;
-		q.pop();
-
-		if (up > 0 && up <= F) {
-
-			if (dist[up] > dist[cur] + 1) {
-				dist[up] = dist[cur] + 1;
-				q.push(up);
-			}
-		}
-
-		if (down > 0 && down <= F)
-		{
-			if (dist[down] > dist[cur] + 1) {
-				dist[down] = dist[cur] + 1;
-				q.push(down);
-			}
-		}
-
-	}
-
-	if (dist[G] != INF)
-		cout << dist[G] << "\n";
-	else
-		cout << "use the stairs" << "\n";
 
 	return 0;
 }
