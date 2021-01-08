9beb7ee5774a94883332ad9132bb18c32fe071f4
wsxc94 <wsxc9468@gmail.com>
2021-01-08 23:56:54 +0900

    bfs

---
+++ Algorithm/2251/물통.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <string>
+#include <string.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+
+	return 0;
+}
