897691dd136d20a3e958906a304835ac9c6d82ff
안영훈 <wsxc9468@gmail.com>
2021-01-28 00:11:36 +0900

    다익스트라 리팩토링

--- Algorithm/4991/해킹.cpp
+++ Algorithm/4991/해킹.cpp
@@ -8,64 +8,72 @@
 
 using namespace std;
 
-struct pos {
-	int idx, val;
-};
-pos tmp;
-struct cmp {
-	bool operator()(pos &a, pos &b) {
-		return a.val > b.val;
-	}
-};
-vector<pos> v[10001];
-
-int dist[10001], node, edge, start, s, e, val, test, cnt, result;
 constexpr int INF = 987654321;
+
+int cnt, time;
+
+void dijkstra(vector<vector<pair<int, int>>> mat, int start, int n) {
+	priority_queue<pair<int, int>> q;
+	vector<int> dist(n + 1, INF);
+
+	q.push({ 0,start });
+	dist[start] = 0;
+
+	while (!q.empty())
+	{
+		int cost = -q.top().first;
+		int cur = q.top().second;
+
+		q.pop();
+
+		for (auto& m : mat[cur])
+		{
+			int next = m.first;
+			int nCost = m.second + cost;
+
+			if (dist[next] > nCost) {
+				dist[next] = nCost;
+				q.push({ -dist[next], next });
+			}
+		}
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		if (dist[i] != INF) {
+			cnt++;
+			if (dist[i] > time) time = dist[i];
+		}
+	}
+}
 int main() {
 	ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
-	cin >> test;
-	for (int t = 0; t < test; t++) {
-		cin >> node >> edge >> start;
-		for (int i = 1; i <= node; i++) {
-			v[i].clear();
-			dist[i] = INF;
+	
+	int t;
+
+	cin >> t;
+
+	while (t--)
+	{
+		int n, d, c;
+		cin >> n >> d >> c;
+
+		vector<vector<pair<int, int>>> mat(n + 1);
+
+		for (int i = 0; i < d; i++)
+		{
+			int a, b, c;
+			cin >> a >> b >> c;
+			mat[b].push_back({ a,c });
 		}
-		for (int i = 0; i < edge; i++) {
-			cin >> e >> s >> val;
-			tmp.idx = e;
-			tmp.val = val;
-			v[s].push_back(tmp);
-		}
-		dist[start] = 0;
-		result = -1;
 		cnt = 0;
-		priority_queue<pos, vector<pos>, cmp> pq;
-		tmp.idx = start;
-		tmp.val = 0;
-		pq.push(tmp);
-		while (!pq.empty()) {
-			int cidx = pq.top().idx;
-			int cv = pq.top().val;
-			pq.pop();
-			if (dist[cidx] < cv) continue;
-			for (int i = 0; i < v[cidx].size(); i++) {
-				int next = v[cidx][i].idx;
-				int nv = v[cidx][i].val;
-				if (dist[next] > cv + nv) {
-					dist[next] = cv + nv;
-					tmp.idx = next;
-					tmp.val = cv + nv;
-					pq.push(tmp);
-				}
-			}
-		}
-		for (int i = 1; i <= node; i++) {
-			if (dist[i] != INF) {
-				cnt++;
-				result = max(result, dist[i]);
-			}
-		}
-		cout << cnt << " " << result << '\n';
+		time = 0;
+
+		dijkstra(mat, c, n);
+
+		cout << cnt << " " << time << "\n";
+
 	}
+
 	return 0;
 }
