83d6ebf197d957a0e6f27d3ddf38bcf151ed1001
안영훈 <wsxc9468@gmail.com>
2021-01-29 23:59:58 +0900

    bfs

---
+++ Algorithm/20055/그림.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <string>
+#include <string.h>
+
+using namespace std;
+
+int dx[] = { 1,-1,0,0 };
+int dy[] = { 0,0,1,-1 };
+
+int main() {
+	ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+
+	return 0;
+}
