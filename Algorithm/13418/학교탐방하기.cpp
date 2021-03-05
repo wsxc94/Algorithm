f3ee53eade7f1e2005c540254c7a34752e434e37
안영훈 <wsxc9468@gmail.com>
2021-03-05 11:13:37 +0900

    Kruskal

--- Algorithm/13418/학교탐방하기.cpp
+++ Algorithm/13418/학교탐방하기.cpp
@@ -9,27 +9,36 @@
 #include <unordered_map>
 #include <deque>
 #include <algorithm>
+#include <cmath>
 using namespace std;
-//크루스칼 알고리즘
+/*
+    최소 스패닝 트리
+    최악의 경로와 최선의 경로를 동시에 찾는 문제
+	크루스칼 알고리즘을 이용하여 최단경로 최대경로 탐색
+	피로도는 오르막길을 K번 오를때마다 k^2이 되기에 제곱후 계산
+*/
 struct Edge
 {
 	int v1;
 	int v2;
 	int val;
 	Edge(int a, int b, int c) : v1(a), v2(b), val(c) {}
-	bool operator < (Edge& b) {
+
+	bool operator < (const Edge& b) {
 		return val < b.val;
 	}
 };
-int n;
-int mat[1001][1001];
+
+int n, m, best, worst;
+
 int unf[1001];
 vector<Edge> ed;
 
 int Find(int v) {
-	if (v == unf[v]) return unf[v];
+	if (unf[v] == v) return unf[v];
 	return unf[v] = Find(unf[v]);
 }
+
 void Union(int a, int b) {
 	a = Find(a);
 	b = Find(b);
@@ -40,40 +49,41 @@ void Union(int a, int b) {
 int main() {
 	ios::sync_with_stdio(false), cin.tie(0), cout.tie(0);
 
-	cin >> n;
+	cin >> n >> m;
 
-	for (int i = 0; i < n; i++) unf[i] = i;
+	ed.reserve(m + 1);
 
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i <= n; i++) unf[i] = i;
+	
+	for (int i = 0; i < m + 1; i++)
 	{
-		for (int j = 0; j < n; j++)
-		{
-			cin >> mat[i][j];
-		}
-	}
-
-	//중복된 연결을 없애기 위함.
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = i+1; j < n; j++)
-		{
-			ed.push_back({ i,j,mat[i][j] });
-		}
+		int a, b, c;
+		cin >> a >> b >> c;
+		ed.push_back(Edge(a, b, c));
 	}
 
 	sort(ed.begin(), ed.end());
-	long long ans = 0;
 
 	for (int i = 0; i < ed.size(); i++)
 	{
-		int a = Find(ed[i].v1);
-		int b = Find(ed[i].v2);
-		if (a != b)
-		{
-			ans += ed[i].val;
+		if (Find(ed[i].v1) != Find(ed[i].v2)) {
 			Union(ed[i].v1, ed[i].v2);
+			if (!ed[i].val) worst++;
 		}
 	}
-	cout << ans << "\n";
+
+	for (int i = 0; i <= n; i++) unf[i] = i;
+
+	for (int i = ed.size() - 1; i >= 0; i--)
+	{
+		if (Find(ed[i].v1) != Find(ed[i].v2)) {
+			Union(ed[i].v1, ed[i].v2);
+			if (!ed[i].val) best++;
+		}
+	}
+
+	cout << pow(worst, 2) - pow(best, 2) << "\n";
+
+
 	return 0;
 }
