b49b3cf673a504963a9dfb1206c3039d9e467ea4
안영훈 <wsxc9468@gmail.com>
2021-08-10 22:28:26 +0900

    위클리 챌린지 1주차 코드 수정

--- Algorithm/프로그래머스 위클리 챌린지 1주차/부족한금액계산하기.cpp
+++ Algorithm/프로그래머스 위클리 챌린지 1주차/부족한금액계산하기.cpp
@@ -12,15 +12,11 @@
 using namespace std;
 /*
    https://programmers.co.kr/learn/courses/30/lessons/82612?language=cpp
-
+   1~n 까지의 합은 n + (n+1) / 2 로 간단히 구할 수 있다.
 */
 long long solution(int price, int money, int count)
 {
-	long long answer = 0;
-
-	for (int i = 1; i <= count; i++) {
-		answer += price * i;
-	}
+	long long answer = 1LL * price * count * (count + 1) / 2;
 
 	if (answer < money) return 0;
 
