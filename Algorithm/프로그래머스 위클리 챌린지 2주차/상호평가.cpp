00d34be5cbefead8d59e8ca2c4e1c336ac40967d
안영훈 <wsxc9468@gmail.com>
2021-08-11 16:46:26 +0900

    위클리 챌린지 2주차 코드 개선

--- Algorithm/프로그래머스 위클리 챌린지 2주차/상호평가.cpp
+++ Algorithm/프로그래머스 위클리 챌린지 2주차/상호평가.cpp
@@ -16,17 +16,16 @@ using namespace std;
    자기 자신이 매긴 점수가 유일하게 제일 큰 수 이거나 제일 작은 수 일 경우 점수를
    제외하고 평균을 매겨야 하기 때문에
    selfScore = 자기자신을 매긴 점수 를 저장해놓아서
-   maxScore , minScore와 같은지 검사한다.
-
+   maxScore 보다 큰지 , minScore 보다 작은지 검사한다.
 
    2021-08-10
    check 배열 초기화를 안해줬던거 수정
+   2021-08-11
+   수식 변경으로 공간복잡도 최적화
 
 */
-int check[101];
 char func(float sum, int size) {
 	float ave = sum / size;
-	//cout << ave << " ";
 	if (ave >= 90) return 'A';
 	else if (ave >= 80) return 'B';
 	else if (ave >= 70) return 'C';
@@ -42,18 +41,13 @@ string solution(vector<vector<int>> scores) {
 		int maxScore = -1;
 		int minScore = 987654321;
 		float sum = 0;
-		vector<int> v;
 		for (int j = 0; j < size; j++) {
-			if (j != i) {
-				check[scores[j][i]] = true;
-				v.push_back(scores[j][i]);
-				sum += scores[j][i];
-			}
+			if (i == j) continue;
+			sum += scores[j][i];
 			maxScore = max(maxScore, scores[j][i]);
 			minScore = min(minScore, scores[j][i]);
-
 		}
-		if ((selfScore == maxScore || selfScore == minScore) && !check[selfScore]) {
+		if (selfScore > maxScore || selfScore < minScore) {
 			answer += func(sum, size - 1);
 
 		}
@@ -61,7 +55,7 @@ string solution(vector<vector<int>> scores) {
 			sum += selfScore;
 			answer += func(sum, size);
 		}
-		for (int a : v) check[a] = false;
+
 	}
 	return answer;
 }
